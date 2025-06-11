3a9b288698ddfc69e57b1b01508a86fa0d039727
Tammam Ali <37336178+TammamAli@users.noreply.github.com>
2025-06-11 15:04:04 +0300

    workerpool

--- concurrency/workerpool.go
+++ concurrency/workerpool.go
@@ -3,6 +3,7 @@ package concurrency
 func WaitGroups() {
 
 	/*
-		WaitGroup: A WaitGroup is used to wait for a collection of Goroutines to finish executing. The control is blocked until all Goroutines finish executing
+		WaitGroup: A WaitGroup is used to wait for a collection of Goroutines to finish executing.
+		The control is blocked until all Goroutines finish executing
 	*/
 }
